153bd73adfe1854a7bbeb2c57fbc878473340536
Cristian Arteaga <cristiandavidarteaga@gmail.com>
2020-07-28 10:33:53 -0700

    Initial commit after moving from pymlogit

---
+++ mixedlogit/__init__.py
@@ -0,0 +1,1 @@
+from .choicemodel import ChoiceModel
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(name='mixedlogit',
+      version='0.0.1',
+      description='Estimation of mixed, multinomial, and conditional logit models',
+      url='https://github.com/arteagac/mixedlogit',
+      author='Cristian Arteaga',
+      author_email='cristiandavidarteaga@gmail.com',
+      license='MIT',
+      packages=['mixedlogit'],
+      zip_safe=False,
+      python_requires='>=3.5',
+      install_requires=[
+          'numpy>=1.13.1',
+          'scipy>=1.0.0'
+      ])
